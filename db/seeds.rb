0006f441bf0933696c430964ea5360a0f8eccb1c
Sophie Watson <semwatson09@gmail.com>
2023-08-23 02:55:01 +0200

    App created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create(name: "Epicure", category: "french", address: "London", phone_number:"0121 242 2688")
+Restaurant.create(name: "Malandrinos", category: "italian", address: "San Sebastian", phone_number:"945 342 587")
+Restaurant.create(name: "Nobu", category: "japanese", address: "Dubai")
+Restaurant.create(name: "WaffleWorld", category: "belgian", address: "London", phone_number:"0121 242 5449")
+Restaurant.create(name: "WokToWalk", category: "chinese", address: "Burton", phone_number:"01543 478 562")
